482a744b01f7c40a0011558783b9bfc8e4d63036
Joseph Gregory <josephcgregory@gmail.com>
2023-06-03 13:35:54 -0600

    modified:   week7/package-lock.json 	modified:   week7/package.json 	deleted:    week7/routes.rest 	modified:   week7/routes/cards.js 	modified:   week7/routes/index.js

--- week7/routes/cards.js
+++ week7/routes/cards.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+const {auth, requiresAuth} = require('express-openid-connect');
 const proContacts = require('../controllers/cardspecifics');
+
 
 const router = express.Router();
 router.get('/', proContacts.getAll);
-router.get('/:id', proContacts.getSingle);
+router.get('/:id',requiresAuth(), proContacts.getSingle);
 router.post('/', proContacts.createCard);
 router.put('/:id',proContacts.changeCard);
 router.delete('/:id',proContacts.deleteCard);
--- week7/routes/index.js
+++ week7/routes/index.js
@@ -1,5 +1,22 @@
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:8080/',
+  clientID: 'MfSSpHsyPGvwwk4iOWgV62eCz9IH2rvi',
+  issuerBaseURL: 'https://dev-rpb3yv2pay34uyin.us.auth0.com/'
+};
+
+
 const express = require('express');
 const router = express.Router();
+
+router.use(auth(config));
+router.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 router.use('/', require('./swagger'));
 router.use('/cards', require('./cards'));
