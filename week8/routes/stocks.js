245b30dc157d7c08b0757c9a89fccc1da1f75105
Joseph Gregory <josephcgregory@gmail.com>
2023-06-10 14:33:06 -0600

    modified:   week8/routes/stocks.js

--- week8/routes/stocks.js
+++ week8/routes/stocks.js
@@ -3,8 +3,8 @@ const {auth, requiresAuth} = require('express-openid-connect');
 const proContacts = require('../controllers/cardspecifics');
 
 const router = express.Router();
-router.get('/', proContacts.getAllStock);
-router.get('/:id',requiresAuth(), proContacts.getSingleStock);
+router.get('/',requiresAuth(), proContacts.getAllStock);
+router.get('/:id',proContacts.getSingleStock);
 router.post('/', proContacts.createCardStock);
 router.put('/:id',proContacts.changeCardStock);
 router.delete('/:id',proContacts.deleteCardStock);
